011f90b7badc35becfc2d0f49e1123972e766ac7
SC-201812191507\Administrator <xu649047654@163.com>
2019-01-19 09:19:05 +0800

    tasks api 提交

---
+++ tests/func/test_api_exceptions.py
@@ -0,0 +1,24 @@
+import pytest
+import tasks
+
+
+
+# 预期异常
+def test_add_raises():
+    """add() should raise an exception with wrong type param"""
+    with pytest.rainses(TypeError):
+        tasks.add(task='not a Task object')
+
+
+def test_start_tasks_db_raises():
+    """Make sure unsupported db raises an exception"""
+    with pytest.raises(ValueError) as excinfo:
+        tasks.start_tasks_db('some/great/path','mysql')
+    exception_msg = excinfo.value.args[0]
+    assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
+
+
+@pytest.mark.smoke
+def test_list_raise():
+    # with
+    pass
